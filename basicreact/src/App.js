38fbb4b0fbeb712fd40b0e8495bf2e48e2289f64
Riyazshaheed <riyaztorgal02@gmail.com>
2025-11-27 20:17:59 +0530

    day 2

--- basicreact/src/App.js
+++
@@ -1,9 +0,0 @@
-
-
-function App() {
-  return (
-    <h1>Hello and welcom to Riyaz React practice</h1>
-  );
-}
-
-export default App;
--- vitereact/src/test.jsx
+++
@@ -1,5 +0,0 @@
-function Test(){
-    return (<h2> developing vite </h2>)
-}
-
-export default Test
